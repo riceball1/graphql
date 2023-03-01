4ad9e6123e5daaa610ea1ccb7ef4ed313adc27fb
Dana Ng <ngdana1@gmail.com>
2023-03-01 14:35:16 -0800

    Update schema

--- graphql-essential-training/index.js
+++ graphql-essential-training/index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import { graphqlHTTP } from 'express-graphql';
+import schema from './schema'
 
 
 const app = express()
@@ -8,6 +10,24 @@ const PORT = 8080;
 app.get('/', (req, res) => {
     res.send('GraphQL')
 })
+
+// initial graphql 
+const root  = { product: () => {
+    return {
+        "id": 124508,
+        "name": "widget",
+        "description": "Beautiful widget",
+        "price": 24.99,
+        "soldout": false,
+    }
+}}
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}))
+
 
 app.listen(PORT, () => {
     console.log(`Running server on localhost:${PORT}/graphql`)
